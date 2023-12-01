01d4223df3aab649fb16d4690b76fa6696647db6
Breno Cocheto <48560880+bcocheto@users.noreply.github.com>
2023-12-01 11:35:20 -0300

    feat: Componentes, Contexto e tema

--- src/componentes/Cabecalho/Cabecalho.jsx
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -3,4 +3,4 @@ import styled from '@emotion/styled'
 export const Cabecalho = styled.header`
     background: ${props => props.theme.cores.primarias.a};
     padding: ${props => props.theme.espacamentos.m};
-`;
+`
